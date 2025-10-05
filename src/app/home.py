9d64664597ca1bf7cdbe8882d52077510064d2fa
albert <micmia128@gmail.com>
2025-10-05 11:22:34 +0200

    Initialize Streamlit with pagination

---
+++ src/app/home.py
@@ -0,0 +1,16 @@
+"""Home page of the app."""
+
+import streamlit as st
+
+st.set_page_config(page_title="Home", page_icon="📈")
+
+st.markdown("# Home")
+st.sidebar.header("Home")
+
+st.markdown(
+    """
+    Welcome to the **Mangetamain**!
+    
+    This application is designed to help you explore healthy recipes.
+    """
+)
