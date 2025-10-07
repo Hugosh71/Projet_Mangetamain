0b9ae68de0914ac0048005586993c3dc26637648
albert <micmia128@gmail.com>
2025-10-07 09:29:48 +0200

    Build docker only when push to main

--- src/mangetamain/__init__.py
+++ src/mangetamain/__init__.py
@@ -2,6 +2,10 @@
 
 __all__ = [
     "__version__",
+    "DataPreprocessor",
+    "get_top_recipes_cached",
 ]
 
 __version__ = "0.1.0"
+
+from .preprocessing import DataPreprocessor, get_top_recipes_cached  # re-export for convenience
