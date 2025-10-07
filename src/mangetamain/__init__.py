56c92a530ac2984c7d6fb177c0d83de73134ab62
albert <micmia128@gmail.com>
2025-10-07 11:14:32 +0200

    Add preprocessing package

--- src/mangetamain/__init__.py
+++ src/mangetamain/__init__.py
@@ -4,8 +4,13 @@ __all__ = [
     "__version__",
     "DataPreprocessor",
     "get_top_recipes_cached",
+    "get_vegetarian_stats_cached",
 ]
 
 __version__ = "0.1.0"
 
-from .preprocessing import DataPreprocessor, get_top_recipes_cached  # re-export for convenience
+from .preprocessing import (
+    DataPreprocessor,
+    get_top_recipes_cached,
+    get_vegetarian_stats_cached,
+)
