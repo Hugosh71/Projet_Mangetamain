e42b790927c4877349ebcc193d4f56277992f8ec
albert <micmia128@gmail.com>
2025-10-16 15:39:33 +0200

    Update streamlit pages

--- src/mangetamain/utils.py
+++ src/mangetamain/utils.py
@@ -1,11 +1,15 @@
 """Utility functions"""
 
+import logging
 from functools import lru_cache
 
 import pandas as pd
 
+logging.basicConfig(level=logging.INFO)
+
 
 @lru_cache(maxsize=5)
-def read_csv(path, sep=",", nrows=None):
+def read_csv(path, usecols: tuple[str, ...] | str | None = None) -> pd.DataFrame:
     """Load a CSV file with caching to optimize repeated reads."""
-    return pd.read_csv(path, sep=sep, nrows=nrows)
+    logging.info("Loading CSV from %s with usecols=%s", path, usecols)
+    return pd.read_csv(path, usecols=list(usecols))
